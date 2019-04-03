74d49de25ef50b37df9885ff960a20c693663805
Di-Chai <dchai@connect.ust.hk>
2019-04-03 11:46:46 +0800

    add data_dir in dataset

--- build/lib/UCTB/dataset/__init__.py
+++
@@ -1,2 +0,0 @@
-from .data_loader import NodeTrafficLoader
-from .dataset import DataSet
--- build/lib/UCTB/model_unit/__init__.py
+++
@@ -1,9 +0,0 @@
-
-from .BaseModel import BaseModel
-
-from .GraphModelLayers import GraphBuilder
-from .GraphModelLayers import GCL
-from .GraphModelLayers import GAL
-
-from .GCLSTM_CELL import GCLSTMCell
-from .GCN_LSTM_CELL import GCN_LSTMCell

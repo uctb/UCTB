dfb88566a9af325be63436341aa37e9c6a2f6ba0
Di-Chai <dchai@connect.ust.hk>
2019-07-04 22:55:49 +0800

    push to master

--- Experiments/AMulti_GCLSTM/find_similar_pairs.py
+++ Experiments/AMulti_GCLSTM/find_similar_pairs.py
@@ -7,12 +7,15 @@ source_city = 'DC'
 target_dataset = 'Bike'
 target_city = 'NYC'
 
+target_data_length = 1  # day
+
 source_data_loader = NodeTrafficLoader(dataset=source_dataset, city=source_city,
                                        closeness_len=6, period_len=7, trend_len=4,
                                        normalize=True, with_lm=False, with_tpe=False,
                                        workday_parser=is_work_day_america)
 
 target_data_loader = NodeTrafficLoader(dataset=target_dataset, city=target_city,
+                                       train_data_length=target_data_length,
                                        closeness_len=6, period_len=7, trend_len=4,
                                        normalize=True, with_lm=False, with_tpe=False,
                                        workday_parser=is_work_day_america)

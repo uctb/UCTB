4899055aa51292dfb5e2f52c63ae2b790a9213a4
xazeon <31511454+xazeon@users.noreply.github.com>
2019-07-11 16:16:07 +0800

    Add docstring for dataset module
    Add docstring for dataset module

--- UCTB/dataset/dataset.py
+++ UCTB/dataset/dataset.py
@@ -5,8 +5,36 @@ import tarfile
 
 
 class DataSet(object):
+    """An object storing basic data from a formatted pickle file.
 
-    def __init__(self, dataset, city, data_dir=None):
+    See also `Build your own datasets <https://di-chai.github.io/UCTB/static/tutorial.html>`_.
+
+    Args:
+        dataset (str): A string containing path of the dataset pickle file or a string of name of the dataset.
+        city (:obj:`str` or ``None``): ``None`` if dataset is file path, or a string of name of the city. Default: ``None``
+        data_dir (:obj:`str` or ``None``): The dataset directory. If set to ``None``, a directory will be created. If
+            ``dataset`` is file path, ``data_dir`` should be ``None`` too. Default: ``None``
+
+    Attributes:
+        data (dict): The data directly from the pickle file. ``data`` may have a ``data['contribute_data']`` dict to
+            store supplementary data.
+        time_range (list): From ``data['TimeRange']`` in the format of [YYYY-MM-DD, YYYY-MM-DD] indicating the time
+            range of the data.
+        time_fitness (int): From ``data['TimeFitness']`` indicating how many minutes is a single time slot.
+        node_traffic (numpy.ndarray): Data recording the main stream data of the nodes in during the time range.
+            From ``data['Node']['TrafficNode']`` with a shape of *L* \* *N* where *L* is the
+            number of time slots and *N* is the number of nodes.
+        node_monthly_interaction (numpy.ndarray): Data recording the monthly interaction of pairs of nodes. Its shape is
+            *M* \* *N* \* *N* where *M* is the number of months in the time range and *N* is the number of nodes. It's
+            from ``data['Node']['TrafficMonthlyInteraction']`` and is used to build interaction graph. It's an optional
+            attribute and can be set as an empty list if interaction graph is not needed.
+        node_station_info (dict): A dict storing the coordinates of nodes. It shall be formatted as {id (may be
+            arbitrary): [id (when sorted, should be consistant with index of ``node_traffic``), latitude, longitude,
+            other notes]}. It's from ``data['Node']['StationInfo']`` and is used to build distance graph. It's an optional
+            attribute and can be set as an empty list if distance graph is not needed.
+    """
+
+    def __init__(self, dataset, city=None, data_dir=None):
 
         self.dataset = dataset
         self.city = city
@@ -17,7 +45,10 @@ class DataSet(object):
         if os.path.isdir(data_dir) is False:
             os.makedirs(data_dir)
 
-        pkl_file_name = os.path.join(data_dir, '{}_{}.pkl'.format(self.dataset, self.city))
+        if self.city is not None:
+            pkl_file_name = os.path.join(data_dir, '{}_{}.pkl'.format(self.dataset, self.city))
+        else:
+            pkl_file_name = self.dataset
 
         if os.path.isfile(pkl_file_name) is False:
             try:

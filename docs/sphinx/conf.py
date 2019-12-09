3cbb3cb1760d5331394b4fc19f8a93aacdf7075d
Di-Chai <dchai@connect.ust.hk>
2019-12-09 21:32:43 +0800

    Change theme

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -88,8 +88,9 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'nature'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = 'rtcat_sphinx_theme'
+import rtcat_sphinx_theme
+html_theme_path = [rtcat_sphinx_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

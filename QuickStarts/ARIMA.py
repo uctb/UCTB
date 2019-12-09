096f7cf9868a560f0af5fadec5885fbbdb81af96
Di-Chai <dchai@connect.ust.hk>
2019-12-09 14:52:35 +0800

    Refresh QuickStart

--- QuickStarts/ARIMA.py
+++ QuickStarts/ARIMA.py
@@ -8,28 +8,20 @@ from UCTB.evaluation import metric
 data_loader = NodeTrafficLoader(dataset='Bike', city='NYC', closeness_len=24, period_len=0, trend_len=0,
                                 with_lm=False, normalize=False)
 
-test_rmse_collector = []
-
+test_prediction_collector = []
 for i in range(data_loader.station_number):
-
     try:
         model_obj = ARIMA(time_sequence=data_loader.train_closeness[:, i, -1, 0],
                           order=[6, 0, 1], seasonal_order=[0, 0, 0, 0])
-
         test_prediction = model_obj.predict(time_sequences=data_loader.test_closeness[:, i, :, 0],
                                             forecast_step=1)
-
     except Exception as e:
         print('Converge failed with error', e)
         print('Using last as prediction')
-
         test_prediction = data_loader.test_closeness[:, i, -1:, :]
+    test_prediction_collector.append(test_prediction)
+    print('Station', i, 'finished')
 
-    test_rmse_collector.append(test_prediction)
-
-    print('Station', i, metric.rmse(test_prediction, data_loader.test_y[:, i:i+1], threshold=0))
-
-test_rmse_collector = np.concatenate(test_rmse_collector, axis=-2)
-test_rmse = metric.rmse(test_rmse_collector, data_loader.test_y, threshold=0)
+test_rmse = metric.rmse(np.concatenate(test_prediction_collector, axis=-2), data_loader.test_y, threshold=0)
 
 print('test_rmse', test_rmse)
--- QuickStarts/HMM.py
+++ QuickStarts/HMM.py
@@ -4,14 +4,12 @@ from UCTB.dataset import NodeTrafficLoader
 from UCTB.model import HMM
 from UCTB.evaluation import metric
 
-data_loader = NodeTrafficLoader(dataset='DiDi', city='Xian',
+data_loader = NodeTrafficLoader(dataset='Bike', city='NYC',
                                 closeness_len=12, period_len=0, trend_len=0,
                                 with_lm=False, normalize=False)
 
 prediction = []
-
 for station_index in range(data_loader.station_number):
-
     # train the hmm model
     try:
         hmm = HMM(num_components=8, n_iter=100)
@@ -26,7 +24,7 @@ for station_index in range(data_loader.station_number):
         p = [[[0]] for _ in range(data_loader.test_closeness.shape[0])]
 
     prediction.append(np.array(p)[:, :, 0])
+    print('Node', station_index, 'finished')
 
 prediction = np.array(prediction).transpose([1, 0, 2])
-
 print('RMSE', metric.rmse(prediction, data_loader.test_y, threshold=0))

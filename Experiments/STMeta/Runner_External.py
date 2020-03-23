c128777f609a4df9204a5bcde1626ed1ec8d28b7
Liyue-Chen <chenliyue2019@gmail.com>
2020-03-23 15:37:31 +0800

    update

--- Experiments/STMeta/Runner_External.py
+++ Experiments/STMeta/Runner_External.py
@@ -20,20 +20,6 @@ import os
 #           '-p graph:Distance-Correlation-Interaction,embedding_flag:True,classified_embedding:True,mark:ClassifiedEmbedding_V1')
 
 
-# os.system('python STMeta_Obj.py -m STMeta_v2.model.yml -d bike_nyc.data.yml '
-#           '-p graph:Distance-Correlation-Interaction')
-# os.system('python STMeta_Obj.py -m STMeta_v2.model.yml -d bike_chicago.data.yml '
-#           '-p graph:Distance-Correlation-Interaction')
-# os.system('python STMeta_Obj.py -m STMeta_v2.model.yml -d bike_dc.data.yml '
-#           '-p graph:Distance-Correlation-Interaction')
-
-
-# os.system('python STMeta_Obj.py -m STMeta_v3.model.yml -d bike_nyc.data.yml '
-#           '-p graph:Distance-Correlation-Interaction')
-# os.system('python STMeta_Obj.py -m STMeta_v3.model.yml -d bike_chicago.data.yml '
-#           '-p graph:Distance-Correlation-Interaction')
-# os.system('python STMeta_Obj.py -m STMeta_v3.model.yml -d bike_dc.data.yml '
-#           '-p graph:Distance-Correlation-Interaction')
 
 
 # ###############################################
@@ -49,57 +35,30 @@ os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d didi_xian.data.yml '
 
 
 
-# os.system('python STMeta_Obj.py -m STMeta_v2.model.yml -d didi_xian.data.yml '
-#           '-p graph:Distance-Correlation-Interaction')
-# #
-# os.system('python STMeta_Obj.py -m STMeta_v0.model.yml -d didi_chengdu.data.yml -p graph:Distance')
-# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d didi_chengdu.data.yml -p graph:Distance')
-# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d didi_chengdu.data.yml -p graph:Correlation')
-# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d didi_chengdu.data.yml -p graph:Interaction')
-# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d didi_chengdu.data.yml '
-#           '-p graph:Distance-Correlation')
-# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d didi_chengdu.data.yml '
-#           '-p graph:Distance-Correlation-Interaction,gcn_k:1,gclstm_layers:1,batch_size:16,mark:K11')
-# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d didi_chengdu.data.yml '
-#           '-p graph:Distance-Correlation-Interaction,gcn_k:1,gclstm_layers:2,batch_size:32,mark:K12')
-# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d didi_chengdu.data.yml '
-#           '-p graph:Distance-Correlation-Interaction,gcn_k:1,gclstm_layers:3,batch_size:32,mark:K13')
-
-# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d didi_chengdu.data.yml '
-#           '-p graph:Distance-Correlation-Interaction,gcn_k:2,gclstm_layers:1,batch_size:16,mark:K21')
-# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d didi_chengdu.data.yml '
-#           '-p graph:Distance-Correlation-Interaction,gcn_k:3,gclstm_layers:1,batch_size:16,mark:K31')
-
 # ###############################################
 # # BenchMark Metro
 # ###############################################
-
-# os.system('python STMeta_Obj.py -m STMeta_v0.model.yml -d metro_chongqing.data.yml'
-#           ' -p graph:Distance,period_len:0,trend_len:0,mark:LSTMC')
-# os.system('python STMeta_Obj.py -m STMeta_v0.model.yml -d metro_shanghai.data.yml'
-#           ' -p graph:Distance,period_len:0,trend_len:0,mark:LSTMC')
-
-# os.system('python STMeta_Obj.py -m STMeta_v0.model.yml -d metro_chongqing.data.yml -p graph:Distance')
-# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d metro_chongqing.data.yml -p graph:Distance')
-# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d metro_chongqing.data.yml -p graph:Correlation')
-# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d metro_chongqing.data.yml -p graph:Line')
-# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d metro_chongqing.data.yml '
-#           '-p graph:Distance-Correlation')
-# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d metro_chongqing.data.yml '
-#           '-p graph:Distance-Correlation-Line')
-# os.system('python STMeta_Obj.py -m STMeta_v2.model.yml -d metro_chongqing.data.yml '
-#           '-p graph:Distance-Correlation-Line')
-
-# os.system('python STMeta_Obj.py -m STMeta_v0.model.yml -d metro_shanghai.data.yml -p graph:Distance')
-# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d metro_shanghai.data.yml -p graph:Distance')
-# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d metro_shanghai.data.yml -p graph:Correlation')
-# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d metro_shanghai.data.yml -p graph:Line')
 # os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d metro_shanghai.data.yml '
-#           '-p graph:Distance-Correlation')
+#           '-p graph:Distance-Correlation-Line,mark:NoExternalfeature')
+
+
 # os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d metro_shanghai.data.yml '
-#           '-p graph:Distance-Correlation-Line')
-# os.system('python STMeta_Obj.py -m STMeta_v2.model.yml -d metro_shanghai.data.yml '
-#           '-p graph:Distance-Correlation-Line')
+#           '-p graph:Distance-Correlation-Line,mark:NotEmbeddinng_V1')
+# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d metro_shanghai.data.yml '
+#           '-p graph:Distance-Correlation-Line,embedding_flag:True,mark:OneLayerEmbeddingV1')
+# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d metro_shanghai.data.yml '
+#           '-p lr:1e-4,graph:Distance-Correlation-Line,embedding_flag:True,mark:OneLayerEmbeddingV1lr4')
+# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d metro_shanghai.data.yml '
+#           '-p lr:1e-4,graph:Distance-Correlation-Line,embedding_flag:True,classified_embedding:True,mark:ClassifiedEmbedding_V1lr4')
+
+
+# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d metro_shanghai.data.yml '
+#           '-p graph:Distance-Correlation-Line,mark:NotEmbeddinng_V1_lre6_400sgd')
+# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d metro_shanghai.data.yml '
+#           '-p graph:Distance-Correlation-Line,mark:NotEmbeddinng_V1_lre6_400')
+# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d metro_shanghai.data.yml '
+#           '-p graph:Distance-Correlation-Line,embedding_flag:True,mark:OneLayerEmbeddingV1_lre6_400')
+
 
 # os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d metro_shanghai.data.yml '
 #           '-p graph:Distance-Correlation-Line,gclstm_layers:1,gcn_k:3,mark:BM13')
@@ -108,13 +67,14 @@ os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d didi_xian.data.yml '
 # # BenchMark ChargeStation
 # ###############################################
 
-# os.system('python STMeta_Obj.py -m STMeta_v0.model.yml -d chargestation_beijing.data.yml'
-#           ' -p graph:Distance,period_len:0,trend_len:0,mark:LSTMC')
 
-# os.system('python STMeta_Obj.py -m STMeta_v0.model.yml'
-#           ' -d chargestation_beijing.data.yml -p graph:Distance')
-# os.system('python STMeta_Obj.py -m STMeta_v1.model.yml'
-#             ' -d chargestation_beijing.data.yml -p graph:Distance')
+os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d chargestation_beijing.data.yml '
+          '-p graph:Distance-Correlation,mark:NotEmbeddinng_V1')
+os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d chargestation_beijing.data.yml '
+          '-p graph:Distance-Correlation,embedding_flag:True,mark:OneLayerEmbeddingV1')
+os.system('python STMeta_Obj.py -m STMeta_v1.model.yml -d chargestation_beijing.data.yml '
+          '-p graph:Distance-Correlation,embedding_flag:True,classified_embedding:True,mark:ClassifiedEmbedding_V1')
+
 # os.system('python STMeta_Obj.py -m STMeta_v1.model.yml'
 #           ' -d chargestation_beijing.data.yml -p graph:Correlation')
 # os.system('python STMeta_Obj.py -m STMeta_v1.model.yml'
